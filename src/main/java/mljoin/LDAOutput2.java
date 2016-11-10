39a26506cf1c0f41711e088cd0405cbe3b7286e4
Jacob Gao <jacobgao@gmail.com>
2016-11-09 18:55:36 -0600

    Removed persist and count

--- src/main/java/mljoin/LDAOutput2.java
+++ src/main/java/mljoin/LDAOutput2.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 public class LDAOutput2 implements Output2 {
 	private static final long serialVersionUID = -6887772726150300326L;
 
-	private ArrayList<LDADelta2> tuples;
+	private ArrayList<LDADelta2a> tuples;
 	
 	public LDAOutput2() {
 		tuples = new ArrayList<>();
 	}
 	
-	public void addTuple(LDADelta2 tuple) {
+	public void addTuple(LDADelta2a tuple) {
 		tuples.add(tuple);
 	}
 	
-	public ArrayList<LDADelta2> getTuples() {
+	public ArrayList<LDADelta2a> getTuples() {
 		return tuples;
 	}
 	
